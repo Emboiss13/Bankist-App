0c2b04632f7b7a118588d367e3695b3acef8a56b
Giuliana <116002662+Emboiss13@users.noreply.github.com>
2025-09-02 18:29:08 +0100

    FixingSortBug

--- Files/script.js
+++ Files/script.js
@@ -102,20 +102,34 @@ const inputClosePin = document.querySelector('.form__input--pin');
 // METHODS
 //-------------------------------------------------
 
-const displayMovements = function (movements, sort = false) {
+const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
-  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  const combinedMovementsDates = acc.movements.map((mov, i) => {
+  return {
+    movement: mov,
+    movementDate: acc.movementsDates[i]
+  };
+});
 
-  movs.forEach(function (mov, i) {
-    const type = mov > 0 ? 'deposit' : 'withdrawal';
+  //const movs = sort ? acc.movements.slice().sort((a, b) => a - b) : acc.movements;
+  if (sort) {
+    combinedMovementsDates.sort((a, b) => a.movement - b.movement);
+  }
+
+  combinedMovementsDates.forEach(function (obj, i) {
+    const {movement, movementDate} = obj;
+    const type = movement > 0 ? 'deposit' : 'withdrawal';
+    const date = new Date(movementDate);
+    const displayDate = date.toLocaleString();
 
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov.toFixed(2)}€</div>
+    <div class="movements__date">${displayDate}</div>
+        <div class="movements__value">${movement.toFixed(2)}€</div>
       </div>
     `;
 
@@ -163,7 +177,7 @@ createUsernames(accounts);
 
 const updateUI = function (acc) {
   // Display movements
-  displayMovements(acc.movements);
+  displayMovements(acc);
 
   // Display balance
   calcDisplayBalance(acc);
@@ -176,6 +190,15 @@ const updateUI = function (acc) {
 // EVENT HANDLERS
 //-------------------------------------------------
 let currentAccount;
+
+//Fake always logged in
+currentAccount = accounts[0];
+updateUI(currentAccount);
+containerApp.style.opacity = 100;
+
+//Login date
+const now = new Date();
+labelDate.textContent = now.toLocaleString(); //date/month/year
 
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
@@ -275,6 +298,6 @@ btnClose.addEventListener('click', function (e) {
 let sorted = false;
 btnSort.addEventListener('click', function (e) {
   e.preventDefault();
-  displayMovements(currentAccount.movements, !sorted);
+  displayMovements(currentAccount, !sorted);
   sorted = !sorted;
 });
